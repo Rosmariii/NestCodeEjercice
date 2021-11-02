3bd8f6d5df516c8ad3bbfa65376760ba6c676ed3
Rosmari <rosmarifernandez@hotmail.com>
2021-11-02 14:54:59 -0300

    añadido auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserDTO } from 'src/user/dto/user.dto';
+import { AuthService } from './auth.service';
+import { LocalAuthGuards } from './guards/local-auth.guards';
+
+@ApiTags('Authentication')
+@Controller('api/v1/auth')
+export class AuthController {
+    constructor( private readonly authService: AuthService) {}
+    
+    @UseGuards(LocalAuthGuards)
+    @Post('signin')
+    async signIn(@Req() req) {
+        return await this.authService.signIn(req.user);
+    }
+
+    @Post('singup')
+    async signUp(@Body() userDTO: UserDTO) {
+        return await this.authService.signUp(userDTO);
+    }
+
+}
